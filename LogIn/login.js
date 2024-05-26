3d7cf311d2fa468fe184118d529cfa6a3a94b22c
Lancherix <lancherix.service@gmail.com>
2024-05-26 11:17:01 -0500

    dqwejk

--- LogIn/login.js
+++ LogIn/login.js
@@ -54,6 +54,7 @@ document.getElementById("submit").addEventListener("click", async function(event
 
       await signInWithEmailAndPassword(auth, email, password);
 
+      alert("ujhyvbgfvryubw");
       window.location.href = "https://www.lancherix.com/home.html";
     } else {
       invalidItem.innerHTML = "*Wrong password or username, try again.";
